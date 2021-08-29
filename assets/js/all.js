4e90b7d271be23f131be58671f417cd212b23f10
jocelyn20065 <jenny22twin1998@gmail.com>
2021-08-29 16:46:18 +0800

    Update 2021-08-29T08:46:13.904Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -12,4 +12,29 @@ var swiper = new Swiper(".mySwiper", {
   },
   grabCursor: true
 });
+var swiper = new Swiper(".SwiperGroup", {
+  slidesPerView: 'auto',
+  grid: {
+    rows: 1,
+    //slidesPerColumn: 2, swiper6寫法
+    fill: 'row' //slidesPerColumnFill: 'row',  swiper6寫法
+
+  },
+  spaceBetween: 10,
+  width: 500,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+      grid: {
+        rows: 2,
+        //slidesPerColumn: 2, swiper6寫法
+        fill: 'row' //slidesPerColumnFill: 'row',  swiper6寫法
+
+      },
+      width: 800
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
