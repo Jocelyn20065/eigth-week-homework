9305c12972151a3f7d90b8b0e8becdbf5d1c0a48
jocelyn20065 <jenny22twin1998@gmail.com>
2021-10-16 15:50:32 +0800

    updated Swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,43 +1,68 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
 
-
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  grabCursor : true,
+const demoSwiper=document.querySelector(".demoSwiper");
  
-});
-
-var swiper = new Swiper(".SwiperGroup", {
-  slidesPerView:'auto',
-  grid: {
-  rows: 1,    //slidesPerColumn: 2, swiper6寫法
-  fill: 'row',//slidesPerColumnFill: 'row',  swiper6寫法
-},
-  spaceBetween: 10,
-  width:500,
-  breakpoints: {
-    767: {
-      slidesPerView:2,
-      slidesPerGroup: 1,
-      spaceBetween:30,
-      grid: {
-        rows: 2,    //slidesPerColumn: 2, swiper6寫法
-        fill: 'row',//slidesPerColumnFill: 'row',  swiper6寫法
-      },
-      width:800,
-    },
-   }
-
-
-
+if(demoSwiper){
+  const swiper = new Swiper(".demoSwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    grabCursor : true,
+   
+  });
   
-});
+}
 
 
+const recommandSwiper=document.querySelector(".recommandSwiper")
+
+if(recommandSwiper){
+  const swiper = new Swiper(".recommandSwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      grid: {
+        rows: 3,
+        fill: 'row',
+      },
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          grid: {
+            rows: 1,
+            fill: 'row',
+          },
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          grid: {
+            rows: 2,
+            fill: 'row',
+          },
+        },
+      },
+    });
+}
+
+
+
+const courseSwiper =document.querySelector(".courseSwiper")
+
+if(courseSwiper){
+  const swiper = new Swiper(".courseSwiper", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    grid: {
+      rows: 3,
+      fill: 'row',
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+      },
+    },
+  });
+}
